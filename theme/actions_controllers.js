cd545fbb8c566a34179df3216dfc919566ac0e3a
Chris Jensen <chris@goodforus.info>
2016-04-24 11:12:14 +0800

    Update controller and add button directive.

--- theme/actions_controllers.js
+++ theme/actions_controllers.js
@@ -2,21 +2,52 @@
 
 (function() {
 angular.module('actionsDemo')
-.controller('ActionsController', ['$log', 'Tabletop', ActionsController]);
-	
+.controller('ActionsController', ['$scope', '$log', 'ActionService', ActionsController])
+
+.directive('actionButton', function ActionButtonDirective() {
+  return {
+    restrict: 'E',
+    templateUrl: 'action_button.html',
+    scope: { action: '=' },
+    controller:  ['$scope', '$element', '$timeout', ActionButtonController]
+  }
+});
+
 /**
   * ActionsController
+  *
+  * *$scope*
+  * * actions - Array of actions to display
   */
-function ActionsController($log, Tabletop) {
-	Tabletop.then(function(TabletopSheets) {
-		var allSheets = TabletopSheets[0];
-		var actionSheet = TabletopSheets[0]["Actions"].all();
+function ActionsController($scope, $log, ActionService) {
+	$scope.actions = [];
 
-		// Iterate over the actions and log them
-		for (var i=0; i<actionSheet.length; i++) {
-		  $log.debug('Action: ' + actionSheet[i]['Name'] +  ', slug: ' + actionSheet[i]['Page Slug']);
-		}
+	ActionService.then(function(actions) {
+		$scope.actions = actions.allActions();
 	});
+}
+
+/**
+  * ActionButtonController
+  *
+  * Adjusts the page_id on the signup form to send the user to the next page
+  */
+function ActionButtonController($scope, $element, $timeout) {
+	setDestinationPage($scope.action);
+ 
+	// Sets the destination page for this action. Only sets it if
+	// action is present
+	function setDestinationPage(action) {
+		// Timeout to give the DOM a chance to render so the
+		// input element can be found
+		$timeout(function() {
+			// Find the page id from the slug
+			var pageID = config.page_map[action['Page Slug']];
+		
+			// Find the hidden form element, update it’s value
+			$element.find("input[name='page_id']").attr('value', pageID);
+		}, 0);
+	}
 }
 
 })();
